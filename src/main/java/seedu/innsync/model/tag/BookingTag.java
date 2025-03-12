f9c70a45a346a8eb99b0e86317b6aa994f2ddeb9
iuhiah <leehaihui2004@gmail.com>
2025-03-12 15:50:09 +0800

    Change booking tag format to support name, start date and end date

--- src/main/java/seedu/innsync/model/tag/BookingTag.java
+++ src/main/java/seedu/innsync/model/tag/BookingTag.java
@@ -12,13 +12,16 @@ import java.time.format.DateTimeFormatter;
  */
 public class BookingTag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Date tag names should in the format of YYYY-MM-DD";
-    public static final String VALIDATION_REGEX = "\\d{4}-\\d{2}-\\d{2}";
+    public static final String MESSAGE_CONSTRAINTS = "Booking tags should be of the format "
+            + "{property} from/{start-date} to/{end-date} "
+            + "where start-date and end-date are in the format yyyy-MM-dd. "
+            + "The start-date should be before the end-date.";
+    public static final String VALIDATION_REGEX = ".* from/\\d{4}-\\d{2}-\\d{2} to/\\d{4}-\\d{2}-\\d{2}";
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-    public final LocalDateTime bookingTagDate;
     public final String bookingTagName;
-
+    public final LocalDateTime startDate;
+    public final LocalDateTime endDate;
 
     /**
      * Constructs a {@code Tag}.
@@ -28,15 +31,25 @@ public class BookingTag {
     public BookingTag(String bookingTagName) {
         requireNonNull(bookingTagName);
         checkArgument(isValidBookingTagName(bookingTagName), MESSAGE_CONSTRAINTS);
+
+        String[] split = bookingTagName.split(" from/| to/");
         this.bookingTagName = bookingTagName;
-        this.bookingTagDate = LocalDateTime.parse(bookingTagName, DATE_TIME_FORMATTER);
+        this.startDate = LocalDateTime.parse(split[1], DATE_TIME_FORMATTER);
+        this.endDate = LocalDateTime.parse(split[2], DATE_TIME_FORMATTER);
     }
 
     /**
      * Returns true if a given string is a valid tag name.
      */
     public static boolean isValidBookingTagName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        if (test.matches(VALIDATION_REGEX)) {
+            String[] split = test.split(" from/| to/");
+            LocalDateTime startDate = LocalDateTime.parse(split[1], DATE_TIME_FORMATTER);
+            LocalDateTime endDate = LocalDateTime.parse(split[2], DATE_TIME_FORMATTER);
+            return startDate.isBefore(endDate);
+        } else {
+            return false;
+        }
     }
 
     @Override
