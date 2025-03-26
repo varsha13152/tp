2df324e7e6cbd641a84075db499df7f729dc3fee
iuhiah <leehaihui2004@gmail.com>
2025-03-26 14:31:08 +0800

    Fix tag command parsing

--- src/main/java/seedu/innsync/logic/parser/TagCommandParser.java
+++ src/main/java/seedu/innsync/logic/parser/TagCommandParser.java
@@ -1,10 +1,11 @@
 package seedu.innsync.logic.parser;
 
-import static java.util.Objects.requireNonNull;
+import static seedu.innsync.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.innsync.logic.parser.CliSyntax.PREFIX_BOOKINGTAG;
 import static seedu.innsync.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 import seedu.innsync.commons.core.index.Index;
 import seedu.innsync.logic.commands.TagCommand;
@@ -24,13 +25,23 @@ public class TagCommandParser implements Parser<TagCommand> {
      */
     @Override
     public TagCommand parse(String args) throws ParseException {
-        requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_BOOKINGTAG, PREFIX_TAG);
+
+        if (!atLeastOnePrefixPresent(argMultimap, PREFIX_BOOKINGTAG, PREFIX_TAG)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE));
+        }
 
         Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
         Set<BookingTag> bookingTagList = ParserUtil.parseBookingTags(argMultimap.getAllValues(PREFIX_BOOKINGTAG));
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
         return new TagCommand(index, tagList, bookingTagList);
+    }
+
+    /**
+     * Returns true if at least one of the prefixes is present in the {@code ArgumentMultimap}.
+     */
+    private static boolean atLeastOnePrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
