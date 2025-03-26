9e308713256af21f8e9714c39f45875e7160abd0
iuhiah <leehaihui2004@gmail.com>
2025-03-27 01:20:10 +0800

    Fix bugs in untag command
    - Error message showing wrong tag
    - Verify only one tag is deleted per command

--- src/main/java/seedu/innsync/logic/parser/UntagCommandParser.java
+++ src/main/java/seedu/innsync/logic/parser/UntagCommandParser.java
@@ -18,30 +18,29 @@ public class UntagCommandParser implements Parser<UntagCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the TagCommand
      * and returns a TagCommand object for execution.
+     * Only allows one tag to be removed at a time.
      * @throws ParseException if the user input does not conform the expected format
      */
     @Override
     public UntagCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_BOOKINGTAG, PREFIX_TAG);
 
-        if (!atLeastOnePrefixPresent(argMultimap, PREFIX_BOOKINGTAG, PREFIX_TAG)) {
+        if (!onlyOnePrefixPresent(argMultimap, PREFIX_BOOKINGTAG, PREFIX_TAG)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UntagCommand.MESSAGE_USAGE));
         }
-        boolean hasBookingTag = argMultimap.getValue(PREFIX_BOOKINGTAG).isPresent();
-        boolean hasTag = argMultimap.getValue(PREFIX_TAG).isPresent();
-        if (hasBookingTag == hasTag) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UntagCommand.MESSAGE_USAGE));
-        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_BOOKINGTAG, PREFIX_TAG);
         Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-        String bookingTag = argMultimap.getValue(PREFIX_BOOKINGTAG).orElse("");
         String tag = argMultimap.getValue(PREFIX_TAG).orElse("");
+        String bookingTag = argMultimap.getValue(PREFIX_BOOKINGTAG).orElse("");
 
         return new UntagCommand(index, tag, bookingTag);
     }
+
     /**
-     * Returns true if at least one of the prefixes is present in the {@code ArgumentMultimap}.
+     * Returns true if only one of the prefixes is present in the {@code ArgumentMultimap}.
      */
-    private static boolean atLeastOnePrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    private static boolean onlyOnePrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).filter(prefix -> argumentMultimap.getValue(prefix).isPresent()).count() == 1;
     }
 }
