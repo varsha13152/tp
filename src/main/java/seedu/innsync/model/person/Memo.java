7f938abe3ddb88c4f1d9a6a24bfadc1ed5f6e525
tanxingzhong <zetter358@gmail.com>
2025-03-31 00:10:01 +0800

    Add validation for memo

--- src/main/java/seedu/innsync/model/person/Memo.java
+++ src/main/java/seedu/innsync/model/person/Memo.java
@@ -1,12 +1,17 @@
 package seedu.innsync.model.person;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.innsync.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Person's Memo in the address book.
  * Guarantees: immutable; is always valid
  */
 public class Memo {
+
+    public static final String MESSAGE_CONSTRAINTS = "Memo names should not contain the '/' character, "
+            + "and must not exceed 255 characters.";
+    public static final String VALIDATION_REGEX = "^[^/]{1,255}$";
     public final String value;
 
     /**
@@ -16,8 +21,17 @@ public class Memo {
      */
     public Memo(String memo) {
         requireNonNull(memo);
+        checkArgument(isValidMemo(memo), MESSAGE_CONSTRAINTS);
         value = memo;
     }
+
+    /**
+     * Returns true if a given string is a valid memo format.
+     */
+    public static boolean isValidMemo(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
 
     @Override
     public String toString() {
