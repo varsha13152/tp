bf662d1313ac175db0c07bbe26f18be353522563
monobeartae <guanquntan@gmail.com>
2025-04-01 17:16:00 +0800

    Add Test for UnstarCommandParser

---
+++ src/test/java/seedu/innsync/logic/parser/UnstarCommandParserTest.java
@@ -0,0 +1,32 @@
+package seedu.innsync.logic.parser;
+
+import static seedu.innsync.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.innsync.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.innsync.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.innsync.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.innsync.logic.commands.UnstarCommand;
+
+/**
+ * As we are only doing white-box testing, our test cases do not cover path variations
+ * outside of the UnstarCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the UnstarCommand, and therefore we test only one of them.
+ * The path variation for those two cases occur inside the ParserUtil, and
+ * therefore should be covered by the ParserUtilTest.
+ */
+public class UnstarCommandParserTest {
+
+    private UnstarCommandParser parser = new UnstarCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsUnstarCommand() {
+        assertParseSuccess(parser, "1", new UnstarCommand(INDEX_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnstarCommand.MESSAGE_USAGE));
+    }
+}
