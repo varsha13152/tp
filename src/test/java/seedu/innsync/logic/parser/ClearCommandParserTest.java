846cc6c69e70b26bf4f1cdfbc6a5256e8a99f687
tanxingzhong <156566446+TanXingZhong@users.noreply.github.com>
2025-04-03 21:47:05 +0800

    Update ClearCommandParserTest.java

--- src/test/java/seedu/innsync/logic/parser/ClearCommandParserTest.java
+++ src/test/java/seedu/innsync/logic/parser/ClearCommandParserTest.java
@@ -10,8 +10,8 @@ import seedu.innsync.logic.parser.exceptions.ParseException;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the StarCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the StarCommand, and therefore we test only one of them.
+ * outside of the ClearCommand code. For example, inputs "" and "1 abc" take the
+ * same path through the ClearCommand, and therefore we test only one of them.
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
