02befc195c7cc018545793482ebbef926b3c2351
iuhiah <leehaihui2004@gmail.com>
2025-04-06 23:44:30 +0800

    Update tag command tests with new error messages

--- src/test/java/seedu/innsync/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/innsync/logic/commands/CommandTestUtil.java
@@ -87,6 +87,10 @@ public class CommandTestUtil {
     public static final EditCommand.EditPersonDescriptor DESC_AMY;
     public static final EditCommand.EditPersonDescriptor DESC_BOB;
 
+    // logger
+    private static final java.util.logging.Logger logger =
+            java.util.logging.Logger.getLogger(CommandTestUtil.class.getName());
+
     static {
         DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
                 .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
@@ -167,6 +171,9 @@ public class CommandTestUtil {
             } catch (CommandException ce) {
                 boolean matches = Arrays.stream(expectedMessages).anyMatch(ce.getMessage()::equals);
                 if (!matches) {
+                    logger.warning("CommandException message does not match any expected messages.");
+                    logger.warning("Expected messages: " + Arrays.toString(expectedMessages));
+                    logger.warning("Actual message: " + ce.getMessage());
                     throw new AssertionError("CommandException message does not match any expected messages.");
                 }
                 throw ce;
