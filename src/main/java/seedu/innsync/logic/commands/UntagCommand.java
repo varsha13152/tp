f2ad087b50fb2fcb2e2140674116ea14a9498ead
iuhiah <leehaihui2004@gmail.com>
2025-04-06 22:56:03 +0800

    Fix untag command messages

--- src/main/java/seedu/innsync/logic/commands/UntagCommand.java
+++ src/main/java/seedu/innsync/logic/commands/UntagCommand.java
@@ -32,11 +32,11 @@ public class UntagCommand extends Command {
             + PREFIX_BOOKINGTAG + "{property} from/{start-date} to/{end-date}\n"
             + "Example: " + COMMAND_WORD + " 1 t/friends";
     public static final String MESSAGE_SUCCESS = String.format(
-            Messages.MESSAGE_COMMAND_SUCCESS, "Tag", "%s has been removed from the contact's tag list!");
+            Messages.MESSAGE_COMMAND_SUCCESS, "Unag", "%s has been removed from the contact's tag list!");
     public static final String MESSAGE_FAILURE_TAG = String.format(
-            Messages.MESSAGE_COMMAND_FAILURE, "Tag", "Contact does not have the tag %s!");
+            Messages.MESSAGE_COMMAND_FAILURE, "Untag", "Contact does not have the tag %s!");
     public static final String MESSAGE_FAILURE_BOOKINGTAG = String.format(
-            Messages.MESSAGE_COMMAND_FAILURE, "BookingTag", "Contact does not have the booking tag %s!");
+            Messages.MESSAGE_COMMAND_FAILURE, "Untag", "Contact does not have the booking tag %s!");
 
     private final Index index;
     private final Tag tag;
